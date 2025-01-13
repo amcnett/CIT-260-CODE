ef0df11eb146cd57668da33ae97b9f84947cf06e
AM <alicia.mcnett@gmail.com>
2025-01-13 17:48:30 -0500

    Update 160_review.py

--- 160_review.py
+++ 160_review.py
@@ -3,12 +3,12 @@ CIT160 REVIEW, Activity 0
 '''
 
 def main():
-  #call get_principle_investment
-  #call stairs
-  #call print_c_to_f
-  #call print_average_from_file
+    #call get_principle_investment
+    #call stairs
+    #call print_c_to_f
+    #call print_average_from_file
 
-  return
+    return
 
 
 '''
@@ -17,8 +17,8 @@ def main():
 
 
 def get_principle_investment(*, future_value=0, rate=0.01, years=1):
-  #put your arithmetic here
-  return
+    #put your arithmetic here
+    return
 
 
 '''
@@ -27,9 +27,9 @@ def get_principle_investment(*, future_value=0, rate=0.01, years=1):
 
 
 def stairs():
-  #write your stairs code here
+    #write your stairs code here
 
-  return
+    return
 
 
 '''
@@ -38,9 +38,9 @@ def stairs():
 
 
 def print_c_to_f():
-  #Write the C to F conversion here
+    #Write the C to F conversion here
 
-  return
+    return
 
 
 '''
@@ -51,8 +51,8 @@ def print_c_to_f():
 def print_average_from_file():
   #Write your code to open and calculate the average of the values in the file
 
-  return
+    return
 
 
 if __name__ == '__main__':
-  main()
+    main()
