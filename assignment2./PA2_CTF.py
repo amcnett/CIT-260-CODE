87e5198eb9d7b015c11fe605a3f8ac00491d81ed
AM <alicia.mcnett@gmail.com>
2025-01-27 13:27:21 -0500

    Add files via upload

---
+++ assignment2./PA2_CTF.py
@@ -0,0 +1,46 @@
+import answers, hashlib
+
+# Programming Assignment 2 - Capture the Flag, with strings
+#
+# Follow the instructions EXACTLY to get the correct answer to submit to the associated PLATO quiz.
+# For each task (1-7) you can check your answers by using the defined functions.
+
+# Load one of the string cases from the file called what_am_i
+# Line 1- case 1
+# Line 2- case 2
+
+# 0
+# Confirm you have a single string with no whitepsace to start
+# Example
+# if answers.case_one_step_zero(string1):
+#   print('String is correct')
+# else:
+#   print('String is wrong')
+
+# 1
+# If the substring 'DEAD' is in the string, remove it
+
+# 2
+# Lowercase all the letters in the string
+
+# 3
+# Reverse the string
+
+# 4
+# Remove all upper and lowercase Z characters.
+
+# 5
+# Remove all odd indexed characters
+# In other words, create a new string with all the even characters
+
+# 6
+# Remove the back half of the string
+
+# 7
+# Split the string on 'ppqq'
+
+# 8 
+# Enter the last english word in the list into the associated PLATO quiz
+
+# 9
+# Submit this file to the appropriate dropbox.
