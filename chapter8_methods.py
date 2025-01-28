52c2d043c8c0521b3b47467fc4a05b75c3c23aeb
AM <alicia.mcnett@gmail.com>
2025-01-28 11:32:51 -0500

    Create chapter8_methods.py

---
+++ chapter8_methods.py
@@ -0,0 +1,25 @@
+# Explore the wonderful world of string methods!
+
+# input validation
+# check type before casting!
+# What is someone's age in dog years
+
+
+# input validation -- consider use of upper() or lower()
+# remove leading or trailing whitespace
+# have someone state Y or N (perform input validation)
+
+# update date formats
+# replace() can be used to update - in date with /
+
+
+# cross site scripting removal
+# Is called sanitizing input
+# remove harmful < and > with &gt; and &lt; (in one line) with replace()
+# Example <script>alert('Hello')</script>
+
+
+# file validation
+# did they give us the right file type
+# check extension with endswith()
+
