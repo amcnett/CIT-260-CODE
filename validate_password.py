52e18b63bdd0fb563bfccd5202e2607ead8d53d3
AM <alicia.mcnett@gmail.com>
2025-01-27 19:09:49 -0500

    Update validate_password.py

--- validate_password.py
+++ validate_password.py
@@ -1,9 +1,15 @@
-# This program gets a password from the user and
-# validates it.
+# This program gets information and reports login name to the user.
+# It also takes the password from the user and validates it.
 
-import login
+import login # reminder of how to use modules!
 
 def main():
+    # Get login name
+    first = input('Enter your first name: ')
+    last = input('Enter your last name: ')
+    idnumber = input('Enter your student ID number: ')
+    print(login.get_login_name(first, last, idnumber))
+
     # Get a password from the user.
     password = input('Enter your password: ')
 
